99ab6f9cc7a0bee1a846bb59d1df75c16129c360
Daniil Murashov <d.murashov@icloud.com>
2024-12-30 18:22:33 -0600

    Did some stuff

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"word-counter/pkg/wordcounter"
+)
+
+func main() {
+	fmt.Printf("%s", wordcounter.HelloWorld()+"\n")
+}
---
+++ pkg/wordcounter/wordcounter.go
@@ -0,0 +1,5 @@
+package wordcounter
+
+func HelloWorld() string {
+	return "Hello, World!"
+}
