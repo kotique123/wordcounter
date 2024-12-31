f8d9a61cc814ffc7fd4baf0e36a70dd1dd4c5fca
Daniil Murashov <d.murashov@icloud.com>
2024-12-31 12:39:40 -0600

    Implemented functions to cleanup an input string from unneeded elements.

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"word-counter/pkg/wordcounter"
-)
-
 func main() {
-	fmt.Printf("%s", wordcounter.HelloWorld()+"\n")
+
 }
--- pkg/wordcounter/wordcounter.go
+++ pkg/wordcounter/wordcounter.go
@@ -1,5 +1,39 @@
 package wordcounter
 
-func HelloWorld() string {
-	return "Hello, World!"
+import "strings"
+
+func ToLowerCase(phrase string) string {
+	return strings.ToLower(phrase)
 }
+
+func RemoveQuotes(phrase string) []rune {
+	runes := []rune(phrase)
+	switch runes[0] {
+	case 39:
+		runes[0] = 32
+		runes[len(runes)-1] = 32
+	}
+	return runes
+}
+
+func CleanupString(phrase []rune) string {
+	clean_string := strings.Trim(string(phrase), "!&@$%^#()\"+-`~.")
+	return clean_string
+}
+
+func RemoveApostrophy(phrase []rune) []rune {
+	if len(phrase) < 1 {
+		return phrase
+	} else {
+		for i := 0; i < len(phrase)-1; i++ {
+			if phrase[i] == 39 && phrase[i-1] == 32 {
+				phrase[i] = 32
+			} else if phrase[i] == 39 && phrase[i+1] == 32 {
+				phrase[i] = 32
+			}
+		}
+	}
+	return phrase
+}
+
+// make sure that the numbers are kept
